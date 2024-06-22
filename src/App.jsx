4a3825b9a288eb9558257183cab7f64e86c20368
mdbilal <mdbilalofficial@gmail.com>
2024-06-23 04:11:38 +0500

    added blogs tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './components/Header.jsx';
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Contact from './pages/Contact.jsx';
 import Projects from './pages/Projects.jsx';
+import Contact from './pages/Contact.jsx';
+import About from './pages/About.jsx';
+import Blogs from './pages/Blogs.jsx';
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={ <Home /> } />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
         <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="about" element={<About />} />
+        <Route path="blogs" element={<Blogs />}></Route>
       </Routes>
     </>
   )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,9 @@
+import {Link} from 'react-router-dom';
+
 function Projects(){
     return (
         <>
-        
+            
         </>
     );
 }
